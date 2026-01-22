610e4a4619110582cb1203ab66c62c220d071086
JuliaHolz <25624180+JuliaHolz@users.noreply.github.com>
2026-01-21 22:04:38 -0500

    uncomment script lines

--- run_all_steps_hold_out_plates_one_perturb.sh
+++ run_all_steps_hold_out_plates_one_perturb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-base_out="/home/jholz/fraenkel_rotation/testing_mcbert_final"
+base_out="/home/jholz/fraenkel_rotation/testing_mcbert"
 input_csv="/home/jholz/fraenkel_rotation/cellpainting_data/101625_up_to_1K.csv"
 perturbation="KPT"
 h5ad_out="$base_out/standardized_originalfeat.h5ad"
@@ -8,23 +8,23 @@ source /home/jholz/.bashrc
 
 
 #convert our csv file to a h5ad file (comment out these lines if you've already run this step for another model in this folder)
-#conda activate pyctominer
-#python ./format_csv_to_h5ad.py -i $input_csv -o $h5ad_out -s standardize
+conda activate pyctominer
+python ./format_csv_to_h5ad.py -i $input_csv -o $h5ad_out -s standardize
 
 conda activate mcBERT
 
 #create a list of features/"HVGs" (comment out these lines if you've already run this step for another model in this folder)
 #this is a file mcBERT expects to exist but is less relevant/could probably be made obsolete since we use all our features (not just highly variable ones)
-#feat_loc="$base_out/feat.csv"
-#python mcBERT/extract_features.py -i $h5ad_out -o $feat_loc 
+feat_loc="$base_out/feat.csv"
+python mcBERT/extract_features.py -i $h5ad_out -o $feat_loc 
 
 #split the features by perturbation and by line/donor into separate files
 #if you've run for one perturbation already you can get rid of this step
 output_split_h5ad="$base_out/h5ad_files/platesep_standardized_originalfeat"
-#python mcBERT/separate_plates_and_treatments.py -i $h5ad_out -o $output_split_h5ad
+python mcBERT/separate_plates_and_treatments.py -i $h5ad_out -o $output_split_h5ad
 
 #unsupervised pretraining step -- same as for individual perturbations
-#python pretrain_one_perturbation.py  --perturb $perturbation --config configs/configs_pretraining/single_perturbation_cellprofiler.yaml -i "$output_split_h5ad/$perturbation" -o $base_out 
+python pretrain_one_perturbation.py  --perturb $perturbation --config configs/configs_pretraining/single_perturbation_cellprofiler.yaml -i "$output_split_h5ad/$perturbation" -o $base_out 
 
 
 #supervised finetuning step (starts off from chkpt50 of pretraining)
